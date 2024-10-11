e52a995a18ebc2ee1cb7d781da4f682be81799b6
jiazheng <635033265@qq.com>
2024-10-12 00:38:59 +0800

    提交命令行第一版内容

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(name = "rcli", version,author,about,long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(Debug, Parser)]
+enum Subcommand {
+    #[command(name = "csv", about = "csv command")]
+    Csv(CsvOpts),
+}
+
+#[derive(Debug, Parser)]
+struct CsvOpts {
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long)]
+    delimiter: char,
+    #[arg(short, long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
     println!("Hello, Rust!");
 }
